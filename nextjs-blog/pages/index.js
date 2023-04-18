e4372903c2a81a43b30c3cda4ec49cd7e26ec094
Bree Armstrong <brianna.m.armstrong@gmail.com>
2023-04-18 15:13:50 -0400

    static rendering

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,8 +1,18 @@
 import Head from 'next/head';
 import utilStyles from '../styles/utils.module.css';
 import Layout, { siteTitle } from '../components/layout';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -11,6 +21,20 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p>Hello, I'm Bree, A Web Developer!</p>
         <p>This is a sample site to learn how to make a react page like this! <a href="https://nextjs.org/learn">Tutorial Here</a>.</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog Section</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date,title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br/>
+              {id}
+              <br/>
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
